fa452accf44f5475b901a69aafe261da64eda779
Richard Ozara <43056476+ozrichards@users.noreply.github.com>
2023-09-25 22:59:25 -0400

    21 restructure root module (#22)
    * Update week1.md
    
    * #21 restructure root module

--- main.tf
+++ main.tf
@@ -1,41 +1,4 @@
- terraform {
-#   backend "remote" {
-#     hostname = "app.terraform.io"
-#     organization = "ozararichard"
-
-#     workspaces {
-#       name = "terra-house-1"
-#     }
-#   }
-# }
-    cloud {
-      organization = "ozararichard"
-      workspaces {
-        name = "terra-house-1"
-    }
-  }
-}
-terraform {
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
-
-
-provider "aws" {
-  # Configuration options
-}
-provider "random" {
-  # Configuration options
-}
-
+ 
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
   lower = true
@@ -49,8 +12,9 @@ resource "aws_s3_bucket" "example" {
   # Bucket Naming Rules
   # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
   bucket = random_string.bucket_name.result
-}
 
-output "random_bucket_name_result" {
-    value = random_string.bucket_name.result
-}
+  tags = {
+    UserUuid = "var.user_uuid"
+  }
+ }
+
