175ec6906e7f1566c02f02d2dcfa3629afe535c7
Richard Ozara <43056476+ozrichards@users.noreply.github.com>
2023-09-27 23:18:56 -0400

    #25 refactor s3 bucket into the new terrahouse_aws module. (#26)

--- main.tf
+++ main.tf
@@ -1,20 +1,26 @@
- 
-# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
-# resource "random_string" "bucket_name" {
-#   lower = true
-#   upper = false
-#   length           = 32
-#   special          = false
+ terraform {
+#   backend "remote" {
+#     hostname = "app.terraform.io"
+#     organization = "ozararichard"
+
+#     workspaces {
+#       name = "terra-house-1"
+#     }
+#   }
 # }
+#     cloud {
+#       organization = "ozararichard"
+#       workspaces {
+#         name = "terra-house-1"
+#     }
+#   }
+  
+}    
 
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs
-resource "aws_s3_bucket" "website_bucket" {
-  # Bucket Naming Rules
-  # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
-  bucket = var.bucket_name
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.user_uuid
+  bucket_name = var.bucket_name
+}
 
-  tags = {
-    UserUuid = "var.user_uuid"
-  }
- }
 
