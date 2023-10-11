716969d576f52f746427c4efa61d560cdb63fb74
Richard Ozara <ozrichards1@gmail.com>
2023-10-11 19:50:54 +0000

    #42 finish implementation of the custom terraform provider

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -5,6 +5,6 @@ provider "local.providers/local/terratowns" {
   version     = "1.0.0"
   constraints = "1.0.0"
   hashes = [
-    "h1:MVjdeefV1Knr6XQ5DnTeUzLFlfrAtsZyBwAF8ItLSe8=",
+    "h1:TM6b7jxhGuzkuH2YK5Rtrr4o2Pkj8jQtFeg7Sr1PFdA=",
   ]
 }
--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@
 }    
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
   token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
   
@@ -39,4 +39,13 @@ provider "terratowns" {
 #   content_version = var.content_version
 # }
 
-
+resource "terratowns_home" "home" {
+  name = "Dashcam fanatics in 2023!"
+  description = <<DESCRIPTION
+A space to point to best dashcams in 2023 in my personal opinion. 
+DESCRIPTION
+  #domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "3fdq3gz.cloudfront.net"
+  town = "gamers-grotto"
+  content_version = 1
+}
