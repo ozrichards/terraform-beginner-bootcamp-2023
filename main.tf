709568f0799380eb13c80a99b45e5005ae314de8
Richard Ozara <43056476+ozrichards@users.noreply.github.com>
2023-10-12 20:22:28 -0400

    #52 test our terrahome on terratowns test town (#48)

--- main.tf
+++ main.tf
@@ -24,28 +24,27 @@
 }    
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = var.terratowns_endpoint
+  user_uuid= var.teacherseat_user_uuid
+  token=var.terratowns_access_token
   
 }
-# module "terrahouse_aws" {
-#   source = "./modules/terrahouse_aws"
-#   user_uuid = var.user_uuid
-#   bucket_name = var.bucket_name
-#   index_html_filepath = var.index_html_filepath
-#   error_html_filepath = var.error_html_filepath
-#   assets_path = var.assets_path
-#   content_version = var.content_version
-# }
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.teacherseat_user_uuid
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
+  assets_path = var.assets_path
+  content_version = var.content_version
+}
 
 resource "terratowns_home" "home" {
-  name = "Dashcam fanatics in 2023!"
+  name = " Vechicle Dashcam in 2023!"
   description = <<DESCRIPTION
-A space to point to best dashcams in 2023 in my personal opinion. 
+A space to point to best dashcams in 2023. 
 DESCRIPTION
-  #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3fdq3gz.cloudfront.net"
-  town = "gamers-grotto"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  #domain_name = "3fd32oq3gz.cloudfront.net"
+  town = "missingo"
   content_version = 1
 }
